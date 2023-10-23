f86ab331f8ce01c4d67734d17317153ae6482ce6
andyra42 <anand.rapolu@gmail.com>
2023-10-22 21:11:46 -0400

    Add existing project files to Git

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Chat with application support and knowledge base documents on your corporate device using GPT models. No data leaves your corporate and 100% private.',
+    author='plutooAI',
+    license='MIT',
+)
